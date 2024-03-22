15d3c63f840f7a45e25c9716694cd931415bc30c
ianfr93 <albitox10@gmail.com>
2024-03-22 01:22:21 -0300

    cambios

--- assets/Js/nav.js
+++ assets/Js/nav.js
@@ -1,44 +1,39 @@
-// nav.js
-
 document.addEventListener("DOMContentLoaded", function() {
-  // Obtener el usuario autenticado desde el almacenamiento local
-  const usuarioAutenticado = JSON.parse(sessionStorage.getItem("usuarioAutenticado"));
+    // Obtener el usuario autenticado desde el almacenamiento local
+    const usuarioAutenticado = JSON.parse(sessionStorage.getItem("usuarioAutenticado"));
 
-  // Verificar si hay un usuario autenticado
-  if (usuarioAutenticado) {
-      // Obtener el nombre del usuario
-      const nombreUsuario = usuarioAutenticado.perfil.nombre;
+    // Verificar si hay un usuario autenticado
+    if (usuarioAutenticado) {
+        // Obtener el nombre del usuario
+        const nombreUsuario = usuarioAutenticado.perfil.nombre;
 
-      // Eliminar cualquier otro elemento de lista con la clase "user-dropdown" si existe
-      const dropdownExistente = document.querySelector("nav ul li.user-dropdown");
-      if (dropdownExistente) {
-          dropdownExistente.remove();
-      }
+        // Crear el nuevo elemento del perfil de usuario
+        const nuevoElementoPerfilUsuario = document.createElement("li");
+        nuevoElementoPerfilUsuario.classList.add("nav-item");
+        nuevoElementoPerfilUsuario.classList.add("dropdown");
+        nuevoElementoPerfilUsuario.classList.add("user-dropdown");
 
-      // Crear el nuevo elemento de lista
-      const nuevoElementoLista = document.createElement("li");
-      nuevoElementoLista.classList.add("user-dropdown");
+        // Crear el contenido del perfil de usuario
+        nuevoElementoPerfilUsuario.innerHTML = `
+            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user fa-sm"></i> ${nombreUsuario}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="./Ver-mi perfil.html">Ver Perfil de Usuario</a>
+            </div>
+        `;
 
-      // Crear el contenido del nuevo elemento de lista
-      nuevoElementoLista.innerHTML = `
-          <a href="#" class="dropdown-toggle-user">
-              <i class="fas fa-user fa-sm"></i> ${nombreUsuario} <i class="fas fa-caret-down"></i>
-          </a>
-          <ul class="dropdown-menu-user" style="display: none;">
-              <li><a href="./Ver-mi perfil.html">Ver Perfil de Usuario</a></li>
-          </ul>
-      `;
+        // Insertar el nuevo elemento del perfil de usuario antes del botón "Cerrar Sesión"
+        const cerrarCajaElemento = document.querySelector(".navbar-nav .nav-item:nth-child(4)");
+        cerrarCajaElemento.parentNode.insertBefore(nuevoElementoPerfilUsuario, cerrarCajaElemento.nextSibling);
 
-      // Insertar el nuevo elemento de lista antes del elemento "Cerrar Sesión"
-      const cerrarSesionElemento = document.querySelector("nav ul li:last-child");
-      cerrarSesionElemento.parentNode.insertBefore(nuevoElementoLista, cerrarSesionElemento);
-
-      // Agregar evento de clic al enlace "dropdown-toggle-user" para mostrar/ocultar el menú desplegable
-      const dropdownToggle = nuevoElementoLista.querySelector('.dropdown-toggle-user');
-      dropdownToggle.addEventListener('click', function(event) {
-          event.preventDefault();
-          const dropdownMenu = nuevoElementoLista.querySelector('.dropdown-menu-user');
-          dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-      });
-  }
+        // Agregar evento de clic al enlace dropdown-toggle para mostrar/ocultar el menú desplegable
+        const dropdownToggle = nuevoElementoPerfilUsuario.querySelector('#navbarDropdownMenuLink');
+        dropdownToggle.addEventListener('click', function(event) {
+            event.preventDefault();
+            const dropdownMenu = nuevoElementoPerfilUsuario.querySelector('.dropdown-menu');
+            dropdownMenu.classList.toggle('show');
+        });
+        
+    }
 });
