5356ae072c60f81b7693d4a25e1aee7abc9c3438
ianfr93 <albitox10@gmail.com>
2024-03-27 00:24:47 -0300

    cambios

--- assets/Js/cierre-de-caja.js
+++ assets/Js/cierre-de-caja.js
@@ -1,58 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Obtener usuarios existentes de localStorage o inicializar como array vacío
-  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+$(document).ready(function () {
+    // Manejar el envío del formulario de cierre de caja
+    $('form').submit(function (event) {
+        event.preventDefault(); 
 
-  // Capturar el formulario de registro
-  const formRegistro = document.getElementById("loginForm");
+        // Obtener los valores del formulario
+        let montoCierre = $('#montoCierre').val();
+        let comentarios = $('#comentarios').val();
 
-  // Manejar el envío del formulario de registro
-  formRegistro.addEventListener("submit", function (e) {
-      e.preventDefault();
-
-      // Obtener valores del formulario
-      const nombreCompleto = document.getElementById("nombreCompleto").value;
-      const usuario = document.getElementById("username").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-
-      // Verificar si el usuario ya existe
-      if (usuarios.some(user => user.usuario === usuario)) {
-          // Mostrar mensaje de error con SweetAlert2
-          Swal.fire({
-              icon: 'error',
-              confirmButtonColor: '#2c5d70',
-              title: 'Error',
-              text: 'El nombre de usuario ya está en uso.'
-          });
-          console.log("Nombre de usuario ya en uso:", usuario); 
-          return;
-      }
-
-      // Crear un nuevo usuario
-      const newUser = {
-          nombreCompleto: nombreCompleto,
-          usuario: usuario,
-          email: email,
-          password: password
-      };
-
-      // Agregar el nuevo usuario a la lista
-      usuarios.push(newUser);
-
-      // Guardar lista actualizada en localStorage
-      localStorage.setItem("usuarios", JSON.stringify(usuarios));
-
-      // Limpiar el formulario
-      formRegistro.reset();
-
-      // Mostrar mensaje de éxito con SweetAlert2
-      Swal.fire({
-         
-          iconHtml: '<i class="fas fa-check-circle" style="color: green;"></i>',
-          confirmButtonColor: '#2c5d70',
-          title: 'Éxito',
-          text: '¡Usuario registrado con éxito!'
-      });
-      console.log("Nuevo usuario registrado:", newUser); 
-  });
+        // Validar el formulario
+        if (montoCierre && comentarios) {
+ 
+            Swal.fire({
+                iconHtml: '<i class="fas fa-check-circle" style="color: green;"></i>',
+                confirmButtonColor: '#2c5d70',
+                title: 'Caja cerrada con éxito',
+                text: '¡Gracias por tu trabajo!',
+            });
+        } else {
+            // Mostrar mensaje de error si no se completaron todos los campos
+            Swal.fire({
+                confirmButtonColor: '#2c5d70',
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Por favor, completa todos los campos.',
+            });
+        }
+    });
 });
